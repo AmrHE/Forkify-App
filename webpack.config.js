39e71c1d610a04247c4165e20520a1aef63c7056
AmrHE <amr.hassan.emam@gmail.com>
2022-01-05 16:07:14 +0200

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: ['./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: './bundle.js'
